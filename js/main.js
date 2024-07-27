7a4010c7e233de1db231dcec6a5efa88c85723be
Mustafor <rmustafo107@gmail.com>
2024-07-27 23:15:34 +0500

    1 dars end

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var quastion1 = "Ismingiz nima ?"
+var quastion2 = "Yoshingiz nechida ?"
+var quastion3 = "HTML manosini nima ?"
+var quastion4 = "CSS ning qanday foydasi bor nima ?"
+var quastion5 = "SACC bilan CSS ning manosi nima ?"
+var quastion6 = "Container nima uchun kerak ?"
+var quastion7 = "Fontning afzalligi ?"
+var quastion8 = "Column nima ?"
+var quastion9 = "Tailwild CSS nima ?"
+var quastion10 = "IMG ga width nimaga kerak ?"
+var quastion11 = "max-width nima ?"
+var quastion12 = "padding nima ?"
+var quastion13 = "Display flex nima ?"
+var quastion14 = "Strong bilan H1 ning manolari nima ?"
+var quastion15 = "Boolen nima ?"
+
+
+var answer1 = prompt(quastion1)
+var answer2 = prompt(quastion2)
+var answer3 = prompt(quastion3)
+var answer4 = prompt(quastion4)
+var answer5 = prompt(quastion5)
+var answer6 = prompt(quastion6)
+var answer7 = prompt(quastion7)
+var answer8 = prompt(quastion8)
+var answer9 = prompt(quastion9)
+var answer10 = prompt(quastion10)
+var answer11 = prompt(quastion11)
+var answer12 = prompt(quastion12)
+var answer13 = prompt(quastion13)
+var answer14 = prompt(quastion14)
+var answer15 = prompt(quastion15)
+
+
+
+
+
+console.log(quastion1 , answer1);
+console.log(quastion2 , answer2);
+console.log(quastion3 , answer3);
+console.log(quastion4 , answer4);
+console.log(quastion5 , answer5);
+console.log(quastion6 , answer6);
+console.log(quastion7 , answer7);
+console.log(quastion8 , answer8);
+console.log(quastion9 , answer9);
+console.log(quastion10 , answer10);
+console.log(quastion11 , answer11);
+console.log(quastion12 , answer12);
+console.log(quastion13 , answer13);
+console.log(quastion14 , answer14);
+console.log(quastion15 , answer15);
